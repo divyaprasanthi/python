85d7eab548df73a014e3a6e8a9f981de70e576a8
divya <divyaprasanthi.k@gmail.com>
2022-04-23 13:12:04 +0530

    second project

---
+++ function.py
@@ -0,0 +1,14 @@
+# def my_function(fname):
+#     print(fname + "refsnes")
+#     my_function("email")
+#     my_function("tobias")
+#     my_function("linus")
+#     print(fname)
+def my_function():
+    print ("hell from a function")
+    my_function()
+    def my_function(fname):
+        print(fname + "Refsnes")
+        my_function("Email")
+        my_function("Tobias")
+        my_function("Linus")
